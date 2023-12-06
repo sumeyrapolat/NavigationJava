62beb0d949ab8d220169d9e0cef0c5efa707bc31
sumyera <180315055@ogr.cbu.edu.tr>
2023-12-06 11:54:28 +0300

    second commit

---
+++ app/src/main/java/com/sumeyra/navigationjava/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sumeyra.navigationjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
